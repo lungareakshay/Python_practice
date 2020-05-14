5c6f14fa83686cf8fc5b2baf5005893594062378
Akshay <lungareakshay@gmail.com>
2020-05-14 23:21:58 +0530

    changes to stack_using_link_list.py

--- stack_using_link_list.py
+++ stack_using_link_list.py
@@ -28,3 +28,58 @@ class node(object):
         set next node
         '''
         self.next = new_next 
+
+class linkedlist(object):
+
+    def linkedlist(self,head=None):
+        self.head = None
+
+    def insert(self,key_value):
+        '''
+        insert element in link list
+        '''
+        temp = node()
+        temp.data = key_value
+
+        if self.head == None:
+            self.head = temp 
+        else:
+            temp.set_next(self.head)
+            self.head = temp
+
+    def delete(self):
+        '''
+        delete element from link list 
+        '''
+        if self.head == None:
+            return -1
+
+        temp = self.head
+        self.head = temp.get_next()
+
+    def print(self):
+        '''
+        print link list 
+        '''
+        temp = self.head 
+
+        while(temp):
+            print(temp.data)
+            temp = temp.get_next()
+
+
+##########################################################################################
+##########################################################################################
+
+user_linklist = linkedlist()
+
+user_linklist.insert(2)
+user_linklist.insert(4)
+user_linklist.insert(6)
+
+user_linklist.print()
+
+user_linklist.remove()
+
+print('\n')
+user_linklist.print()
