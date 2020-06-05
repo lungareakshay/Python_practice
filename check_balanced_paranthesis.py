023dce6d92a50b202ae43bf66781edf5363f329f
Akshay <lungareakshay@gmail.com>
2020-06-06 00:15:46 +0530

    Final code changes for balanced paranthesis checking problem.

--- check_balanced_paranthesis.py
+++ check_balanced_paranthesis.py
@@ -13,11 +13,46 @@ d = {(a) * [e + f]}
 g = {[(d)]} + (m * n) - [(v)]
 '''
 
-for _ in ls_expression:
-    print(_)
+def check_balanced():
+    '''
+    Check for balanced Paranthesis
+    Return 1 : Expression is balanced 
+    Return -1 : Expression is not balanced
+    '''
+    user_stack = deque()
 
-user_stack = deque()
+    for _ in ls_expression:
 
-user_stack.append('test')
+        # Insert opening paranthesis
+        if _ in ls_start:
+            user_stack.append(_)
+            
+        # Pop paired paranthesis
+        if _ in ls_end:
+            if user_stack:
+                ls_top = user_stack[-1]
+               
+                if _ == ')' and ls_top == '(':
+                    pass 
+                elif ( _ == ']' and ls_top == '['):
+                    pass 
+                elif (_ == '}' and ls_top == '{'):
+                    pass 
+                else: 
+                    return -1
+                user_stack.pop()
+            else:
+                return -1
+            
+    # Finally check for empty stack
+    if user_stack:
+        return -1
+    else:
+        return 1
 
-print(user_stack)
+           
+# call function 
+if check_balanced() == 1 :
+    print('Expression is Balanced')
+else:
+    print('Expression is not Balanced')
